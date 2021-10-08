da36555d81232cab735a54cd00c52f8c0e53a38f
Cyril TOURNIER <cyril.tournier@yachtsolutions.fr>
2021-10-08 12:14:43 +0200

    WIP

--- index.js
+++ index.js
@@ -2,26 +2,17 @@ let grille = {1:null,2:null,3:null,4:null,5:null,6:null,sum:null,bonus:null};
 let status = 'nouveau';
 class Game {
 
-    static CalculateOnes(roll)
+    CalculateOnes(roll)
     {
-        return CalculateNumberCombination(roll, 1);
+        return this.CalculateNumberCombination(roll, 1);
     }
 
-public int CalculateTwos(int[] roll)
-{
-    return CalculateNumberCombination(roll, 2);
-}
+    CalculateNumberCombination(roll, diceValue)
+    {
+        return diceValue * roll.filter(x => x===diceValue).length;
+    }
 
-public int CalculateThrees(int[] roll)
-{
-    return CalculateNumberCombination(roll, 3);
-}
-
-private static int CalculateNumberCombination(int[] roll, int diceValue)
-{
-    return diceValue * roll.Count(dice => dice == diceValue);
-}
-    static partieStatut()
+    partieStatut()
     {
         for (const property in grille) {
             if (grille[property] != null) {
@@ -38,7 +29,7 @@ private static int CalculateNumberCombination(int[] roll, int diceValue)
 
     }
 
-    static rollDice(n){
+    rollDice(n){
         let result = []
         for (var i=1; i<=n; i++) {
             var randomDice = Math.floor(6*Math.random())+1;
@@ -48,7 +39,7 @@ private static int CalculateNumberCombination(int[] roll, int diceValue)
         this.registerDice(result)
     }
 
-    static registerDice(dice)
+    registerDice(dice)
     {
         // for (const d in dice) {
         //     if (grille[property] != null) {
