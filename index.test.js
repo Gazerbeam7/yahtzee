0bc0ec62e12bc8cef0ec8acc24c7b8e427144234
Cyril TOURNIER <cyril.tournier@yachtsolutions.fr>
2021-10-08 17:50:58 +0200

    WIP

--- index.test.js
+++ index.test.js
@@ -1,19 +1,19 @@
 var Game = require("./index.js");
 new Game;
 
-describe('getDiceResult', () => {
-    it('CalculateOnes', () => {
-
-      // const result = Game.calculate(1, [1,1,0,4,4]);
-      const result = Game.calculate('ThreeOfAKind', [1,1,1,4,4]);
-      console.log(result)
-      expect(result).toBe(11);
-    });
-  });
+// describe('getDiceResult', () => {
+//     it('CalculateOnes', () => {
+//
+//       // const result = Game.calculate(1, [1,1,0,4,4]);
+//       const result = Game.calculate('ThreeOfAKind', [1,1,1,4,4]);
+//       console.log(result)
+//       expect(result).toBe(11);
+//     });
+//   });
 
   const data = [
     {
-      expected: 1,
+      expected: 5,
       roll: [1, 1, 1, 1, 1]
     },
     {
@@ -21,16 +21,14 @@ describe('getDiceResult', () => {
       roll: [1, 1, 2, 2, 2],
     },
     {
-      expected: 3,
+      expected: 1,
       roll: [1, 2, 3, 3, 5],
     },
   ];
-
-  describe.each(data)(`A triangle`, (triangle) => {
-    it(`whose roll are ${triangle.roll} should be ${triangle.expected}`, () => {
-      const expected = getTriangleType(...triangle.roll);
-
-      expect(type).toBe(triangle.type);
-    });
+describe.each(data)(`La somme de 1`, (d) => {
+  it(`du lancer ${d.roll} devrait être ${d.expected}`, () => {
+    const result = Game.calculate(1, d.roll)
+    expect(result).toBe(d.expected);
   });
+});
 
